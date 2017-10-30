bc12de147612acebe65fdf4bdda4096ff5f46cf6
L2jLiga <L2jLiga@mail.ru>
2017-10-30 23:40:56 +0700

    Add unzip functionality

--- bin/xd2svg-cli.js
+++ bin/xd2svg-cli.js
@@ -1,5 +1,46 @@
-(() => {
-	'use strict';
+#!/usr/bin/env node
 
-	const artBoardConverter = require('../lib/artBoardConverter.js');
+(() => {
+  'use strict';
+
+  const [fs, unzip] = [
+    require('fs'),
+    require('unzip'),
+  ];
+
+  const artBoardConverter = require('../lib/artBoardConverter.js');
+
+  const inputFile = process.argv[2];
+  let outputFile = process.argv[3];
+
+  if (!inputFile) {
+    console.error('No input file specified!');
+    console.log('Usage: xd2svg input.xd [output.svg]');
+
+    return;
+  }
+
+  if (!outputFile) {
+    let inputName = inputFile.split('.');
+
+    if (inputName.length > 1) {
+      inputName.pop();
+    }
+
+    inputName.push('svg');
+
+    outputFile = inputName.join('.');
+  }
+
+  let input = fs.createReadStream(inputFile);
+
+  input.pipe(unzip.Extract({path: `tmp-${inputFile}`}));
+
+
+
+  // TODO: Write logic this
+
+  input.close();
+
+  console.log(inputFile, outputFile);
 })();
