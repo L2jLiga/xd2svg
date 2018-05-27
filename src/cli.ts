29d66d561aeba957ecc8d29f4ea8a942cc916eb4
L2jLiga <L2jLiga@gmail.com>
2018-05-27 18:27:09 +0700

    Hotfix

--- src/cli.ts
+++ src/cli.ts
@@ -2,22 +2,24 @@ import xd2svg from './cli/xd2svg';
 
 const inputFileName = process.argv[2];
 let outputFileName = process.argv[3];
-if (!inputFileName) {
-  throw new Error('Input file specify!');
-}
 
-if (!outputFileName) {
-  const inputName = inputFileName.split('.');
+if (inputFileName) {
+  if (!outputFileName) {
+    const inputName = inputFileName.split('.');
 
-  if (inputName.length > 1) {
-    inputName.pop();
+    if (inputName.length > 1) {
+      inputName.pop();
+    }
+
+    inputName.push('html');
+
+    outputFileName = inputName.join('.');
   }
 
-  inputName.push('html');
+  xd2svg(inputFileName, outputFileName);
 
-  outputFileName = inputName.join('.');
+  console.log(inputFileName, outputFileName);
+  
+} else {
+	console.log('Usage: xd2svg-cli InputFile.xd [OutputFile.html]')
 }
-
-xd2svg(inputFileName, outputFileName);
-
-console.log(inputFileName, outputFileName);
