63ef65af775654c0ce8c46d31cbabfe3167e130d
L2jLiga <L2jLiga@gmail.com>
2018-06-16 18:40:08 +0700

    Added: License header in each file

--- src/cli/lib/styles/font.ts
+++ src/cli/lib/styles/font.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Andrey Chalkin <L2jLiga>. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/L2jLiga/xd2svg/LICENSE
+ */
+
 import { Parser } from './index';
 
 // tslint:disable:object-literal-sort-keys
