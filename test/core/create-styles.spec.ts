0f1dc7f20da0fd23a2dd6c132fb1de9ee20f9e65
Andrey Chalkin <L2jLiga@gmail.com>
2018-07-17 14:25:32 +0700

    Improve coverage (#16)
    * Exclude models from coverage
    
    * Tests for create-styles
    
    * Fixed test for create-style
    
    * tests for svgo
    
    * tests for resources parser

---
+++ test/core/create-styles.spec.ts
@@ -0,0 +1,40 @@
+import { assert }       from 'chai';
+import { spy, stub }    from 'sinon';
+import { createStyles } from '../../src/core/create-styles';
+import parsers          from '../../src/core/styles';
+
+describe('Core > Create styles', () => {
+  it('should warn if unsupported style', () => {
+    const stylesSrc = {
+      unknownStyle: {},
+    };
+
+    const consoleSpy = stub(console, 'warn');
+
+    const result = createStyles(stylesSrc, null, null, null);
+
+    assert.equal(result, '');
+    assert.equal(consoleSpy.called, true);
+    assert.equal(consoleSpy.callCount, 1);
+
+    consoleSpy.restore();
+  });
+
+  it('should use parser if it present for current style', (done) => {
+    const stylesSrc = {
+      display: {},
+    };
+
+    const parsersSpy = spy(parsers.display, 'parse');
+
+    createStyles(stylesSrc, null, null, null);
+
+    if (parsersSpy.callCount) {
+      done();
+
+      return;
+    }
+
+    done('Test failed: parser wasn\'t used');
+  });
+});
