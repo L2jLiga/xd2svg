69916bcb3e994a7473f0f1e36da16f55817c0d4a
L2jLiga <L2jLiga@gmail.com>
2018-10-21 16:37:15 +0700

    Simplify filter parser

--- src/core/styles/filters.ts
+++ src/core/styles/filters.ts
@@ -18,39 +18,20 @@ export const filters: Parser = {
 
 function filtersParser(src: any[], defs: XMLElementOrXMLNode): string {
   const filter = defs.element('filter');
-  const filterNo = ((defs as any).children as []).length;
+  const filterNo = elementChildrenCount(defs);
   let filterId: string = `filter-${filterNo}-`;
 
   src.reverse().forEach((filterDesc: any, index: number) => {
     const filterName = filterDesc.type.includes('#blur') ? 'blur' : camelToDash(filterDesc.type);
-    const filterParams = filterDesc.params && filterDesc.params[filterDesc.type + 's'] || filterDesc.params || {};
+    const filterParams = getFilterParams(filterDesc);
 
-    if (filterDesc.visible === false || filterParams.visible === false) return;
+    if (filterInvisible(filterDesc)) return;
 
     switch (filterName) {
       case 'blur': {
         filterId += `blur-${filterParams.blurAmount}-${filterParams.brightnessAmount}`;
 
-        filter
-          .element('feGaussianBlur', {
-            in: 'SourceGraphic',
-            result: `blur-${filterNo}-${index}`,
-            stdDeviation: filterParams.blurAmount,
-          }).up()
-          .element('feComponentTransfer', {in: `blur-${filterNo}-${index}`, result: `blur-${filterNo}-${index}`})
-          .element('feFuncR', {type: 'linear', slope: filterParams.brightnessAmount / 100}).up()
-          .element('feFuncG', {type: 'linear', slope: filterParams.brightnessAmount / 100}).up()
-          .element('feFuncB', {type: 'linear', slope: filterParams.brightnessAmount / 100}).up()
-          .element('feFuncA', {type: 'linear', slope: filterParams.fillOpacity}).up();
-
-        if (filterParams.fillOpacity !== 0) {
-          filter
-            .element('feMerge')
-            .element('feMergeNode', {in: `blur-${filterNo}-${index}`}).up()
-            .element('feMergeNode', {in: 'SourceGraphic'});
-        }
-
-        break;
+        return makeBlurFilter(filter, filterParams, `${filterNo}-${index}`);
       }
 
       case 'drop-shadow': {
@@ -75,13 +56,47 @@ function filtersParser(src: any[], defs: XMLElementOrXMLNode): string {
     }
   });
 
-  filter.attribute('id', filterId);
-
-  if (!(filter as any).children.length) {
+  if (!elementChildrenCount(filter)) {
     filter.remove();
 
     return null;
   }
 
+  filter.attribute('id', filterId);
+
   return `url(#${filterId})`;
+}
+
+function elementChildrenCount(ele: any): number {
+  return ele.children.length;
+}
+
+function getFilterParams(filterDesc: any): any {
+  return filterDesc.params && filterDesc.params[filterDesc.type + 's'] || filterDesc.params || {};
+}
+
+function filterInvisible(filterDesc: any): boolean {
+  return filterDesc.visible === false || filterDesc.params && filterDesc.params.visible === false;
+}
+
+// TODO: Still work not so fine as I expect
+function makeBlurFilter(filter: XMLElementOrXMLNode, filterParams: any, filterPostfix: string): void {
+  filter
+    .element('feGaussianBlur', {
+      in: 'SourceGraphic',
+      result: `blur-${filterPostfix}`,
+      stdDeviation: filterParams.blurAmount,
+    }).up()
+    .element('feComponentTransfer', {in: `blur-${filterPostfix}`, result: `blur-${filterPostfix}`})
+    .element('feFuncR', {type: 'linear', slope: filterParams.brightnessAmount / 100}).up()
+    .element('feFuncG', {type: 'linear', slope: filterParams.brightnessAmount / 100}).up()
+    .element('feFuncB', {type: 'linear', slope: filterParams.brightnessAmount / 100}).up()
+    .element('feFuncA', {type: 'linear', slope: filterParams.fillOpacity}).up();
+
+  if (filterParams.fillOpacity !== 0) {
+    filter
+      .element('feMerge')
+      .element('feMergeNode', {in: `blur-${filterPostfix}`}).up()
+      .element('feMergeNode', {in: 'SourceGraphic'});
+  }
 }
