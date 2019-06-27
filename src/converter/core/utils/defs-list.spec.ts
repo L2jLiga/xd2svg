6f2f8f116cd81565ab76c05466f345e5a9861b2a
Andrey Chalkin <andrey.chalkin@sidenis.com>
2019-06-27 18:32:03 +0700

    Added more unit tests

---
+++ src/converter/core/utils/defs-list.spec.ts
@@ -0,0 +1,8 @@
+import * as assert from 'assert';
+import { defs } from './defs-list';
+
+describe('Definitions list', () => {
+  it('should export definitions list', () => {
+    assert.ok(defs);
+  });
+});
