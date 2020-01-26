f8a47ceb798d00db5353da06e3acc5e0596ec8f4
Andrey Chalkin <andrey.chalkin@sidenis.com>
2020-01-26 14:02:25 +0700

    chore: remove mkdirp dependency

--- src/cli/convert-xd.ts
+++ src/cli/convert-xd.ts
@@ -6,8 +6,7 @@
  * found in the LICENSE file at https://github.com/L2jLiga/xd2svg/LICENSE
  */
 
-import { writeFile }  from 'fs';
-import * as mkdirp    from 'mkdirp';
+import { mkdirSync, writeFile } from 'fs';
 import * as logger    from '../common/utils/logger';
 import xd2svg         from '../converter/xd2svg';
 import { CliOptions } from './models';
@@ -17,18 +16,18 @@ const sanitize = require('sanitize-filename');
 export async function convertXd(input: string, options: CliOptions): Promise<void> {
   const svgImages = await xd2svg(input, options);
 
-  await preparePath(options);
+  preparePath(options);
 
   typeof svgImages === 'string' ?
     writeFile(options.output, svgImages, errorHandler)
     : Object.keys(svgImages).map((key) => writeFile(`${options.output}/${sanitize(key)}.svg`, svgImages[key], errorHandler));
 }
 
-async function preparePath(options: CliOptions): Promise<void> {
+function preparePath(options: CliOptions): void {
   const path = options.output.replace(/\\+/g, '/').split('/');
   if (options.single) path.pop();
 
-  await mkdirp(path.join('/'));
+  mkdirSync(path.join('/'), { recursive: true, mode: 0o777 & (~process.umask()) })
 }
 
 function errorHandler(error): void {
